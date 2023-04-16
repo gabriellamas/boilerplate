6ed2f0a5da047af58eeb35aec2e564c179f808b7
Gabriel Lamas <gabrielldarts@gmail.com>
2023-04-16 18:28:11 -0300

    add jest

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,10 +16,7 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
+          <h1>welcome to next.js!</h1>
           <div>
             <a
               href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
